48c8879520c0079a09da07677e1c236ef33c4dc0
dslztx <dslztx@gmail.com>
2019-01-09 00:15:57 +0800

    bug fix

--- dp/lis/lis.cpp
+++ dp/lis/lis.cpp
@@ -1,7 +1,7 @@
 /*
  * 当前已求出的最长上升子序列长度为len。
- * 先判断s[i]与a[len]。若a[len]<=s[i]，则将s[i]接在a[len]后将得到一个更长的上升子序列；
- * 否则，在a[1]..a[len]中，找到最大的j，满足a[j] <= s[i]。令k = j + 1，此时必有a[j] <= s[i] <= a[k]，将s[i]接在a[j]后将得到一个更长的上升子序列，同时更新a[k] =s[i]。
+ * 先判断s[i]与a[len]。若a[len]<s[i]，则将s[i]接在a[len]后将得到一个更长的上升子序列；
+ * 否则，在a[1]..a[len]中，找到最大的j，满足a[j] < s[i]。令k = j + 1，此时必有a[j] < s[i] <= a[k]，更新a[k] =s[i]。
  *
  * 时间复杂度：O(nlogn)
  */
@@ -15,7 +15,7 @@ using namespace std;
 const int MAXN = 1005;
 int h[MAXN], q[MAXN];
 
-//dp[i]表示选择i后最长的序列
+//dp[i]表示以i为末尾元素时最长上升子序列的长度
 int LIS(int n, int *dp) {
     int l, r, mid, len = 1;
     q[len] = h[0], dp[0] = len;
--- dp/lis/lis2.cpp
+++ dp/lis/lis2.cpp
@@ -1,27 +1,27 @@
 /*
  * 求LIS方案数模板
  *
- * 也可以在LIS朴素算法中直接嵌入以下流程，从而提高效率，否则如果采用O(nlogn)的LIS算法和本算法，//那么时间复杂度为O(nlogn+n^2)，而嵌入朴素算法，时间复杂度为O(n^2)
+ * 如果采用O(nlogn)的LIS算法和本算法，那么时间复杂度为O(nlogn+n^2)
  *
- * 输出一种方案，只需O(n)的时间复杂度，从后向前遍历，根据dp进行输出。
+ * 输出一种方案，只需O(n)的时间复杂度，从后向前遍历，根据q数组进行输出。
  */
-int Cal(int n, int len, int *dp)//dp[i]表示选择i后最长的序列
+int Cal(int n, int len, int *dp)//dp[i]表示以i为末尾元素时最长上升子序列的长度
 {
     int number[MAXN], num, flag;
     for (int i = 0; i < n; i++) {
-        num = flag = 0;
+        num = 0;
         for (int j = i - 1; j >= 0; j--) {
             if (cmp(h[j], h[i])) {
-                flag = 1;
                 if (dp[j] == dp[i] - 1)
                     num += number[j];
             } else if (h[j] == h[i])//求不同方案数，加这个判断，5 4 3 4 3只有1种方案；否则不加，有3种方案
             {
-                flag = 1;
-                break;
+                if (dp[j] == dp[i])
+                    dp[j] = 0;
             }
         }
-        if (flag == 0) num = 1;
+        if (num == 0) num = 1;
+
         number[i] = num;
     }
     num = 0;
@@ -30,4 +30,3 @@ int Cal(int n, int len, int *dp)//dp[i]表示选择i后最长的序列
             num += number[i];
     return num;
 }
-
